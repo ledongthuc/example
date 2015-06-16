f1332bcc79b18462a0403dd793e5a39cb5581c2e
thuc <ledongthuc@gmail.com>
2015-06-16 15:45:08 +0700

    Update: example for library assert.h

---
+++ c-ansci-standard-library/assert.h/README.md
@@ -0,0 +1,3 @@
+# Assert.h
+
+Assert.h is one of standard library in C. It's provide way to verify assumtions. If assumtion is fail, it's print a diagnostic message.
---
+++ c-ansci-standard-library/assert.h/main.c
@@ -0,0 +1,17 @@
+#include <assert.h>
+
+/*
+ * main.c
+ *
+ *  Created on: Jun 16, 2015
+ *      Author: chp
+ *
+ *  Use to example assert library
+ */
+
+
+int main(char* args) {
+	assert(10 < 11); 	// pass.
+	assert(10 == 10); 	// pass.
+	assert(10 > 11);	// fail and through error to standard screen.
+}
