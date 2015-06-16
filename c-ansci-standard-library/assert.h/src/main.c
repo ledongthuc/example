2870c13f3d1766ea7bd8a13993faa84ce67732ef
thuc <ledongthuc@gmail.com>
2015-06-16 15:53:38 +0700

    Update: move source file to src folder

--- c-ansci-standard-library/assert.h/src/main.c
+++ c-ansci-standard-library/assert.h/src/main.c
@@ -9,8 +9,7 @@
  *  Use to example assert library
  */
 
-
-int main(char* args) {
+int main() {
 	assert(10 < 11); 	// pass.
 	assert(10 == 10); 	// pass.
 	assert(10 > 11);	// fail and through error to standard screen.
